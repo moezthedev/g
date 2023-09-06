119521e1459c71311a61efdeb7f736d8bd0c9ac1
Moiz Ahsan <moizahsan5@gmail.com>
2023-09-06 11:25:24 +0500

    new commit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import {loginRouter} from "./routes/login.js"
 import {logoutRouter} from "./routes/logout.js"
 import {registerRouter} from "./routes/register.js"
 import {connectMongoDB} from "./data/mongodb.js"
+import cors from "cors"
 import cookieParser from "cookie-parser";
+import functions from "firebase-functions"
 const app = express();
 
 //Connect Mongodb
@@ -12,11 +14,14 @@ connectMongoDB()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors())
 app.use("/login",loginRouter)
 app.use("/register",registerRouter)
 app.use("/logout",logoutRouter)
+
 //Listen
 
-app.listen(process.env.PORT||5000,()=>{
+app.listen(5000,()=>{
     console.log("Server is running on port 5000");
 })
+exports.api = functions.https.onRequest(app)
